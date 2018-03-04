7adfdf9b0cc82d6fe07fde03b0c97b3af2f44370
pvalchev1 <34414816+pvalchev1@users.noreply.github.com>
2018-03-04 16:10:55 +0200

    Add files via upload

---
+++ Hangman G2.php
@@ -0,0 +1,21 @@
+<?php  
+$word='sweeter';
+$letter=$_POST['letter'];
+$replase=' _ ';
+
+
+	$wordL=str_split($word);
+		for ($i=0; $i < count($wordL); $i++) { 
+			if ($wordL[$i]!=$letter) {
+				$wordL[$i]=$replase;
+			}
+		}
+	$strigL[$j]=implode ('', $strigL[$j]);
+	echo "<pre>".print_r($strigL[$j], true)."</pre>";
+$stringArr[$j]=$strigL[$j];
+
+$stringNew=implode (' ',$stringArr);
+echo "$stringNew";
+
+
+?>
