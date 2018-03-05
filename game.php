b63e4ff28c25a7d1052e771f6693f1c6e89eaa9c
ciconianigra <33747301+ciconianigra@users.noreply.github.com>
2018-03-05 21:15:36 +0200

    Add files via upload

---
+++ game.php
@@ -0,0 +1,18 @@
+<?php 
+include ('includes/header.php');
+?>
+			<p>Здравей, ............</p>
+			<p>Познай, кой е този ................... град в България?</p>
+			<p>За целта въведи в полето буква от азбуката.</p>
+				<form method="post" action="hangman.php">
+					<input type="text" name="letter">
+					<input type="submit" value="Въведи">
+				</form>
+			<img src="">	
+			<p>Грешки ... бр.</p>
+			<p>Сгрешени букви</p>
+			<a href="">Нова игра</a>
+		<h3>Класиране</h3>
+
+</body>
+</html>
